f94dcb0f596362199e2a440078f1e7fa7aadcd0a
Ahmed Taha <ahmed.taha@miovision.com>
2025-03-10 14:56:31 +0200

    project react

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -1,19 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 export const CartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
+    numOfItems: 0,
   },
+
   reducers: {
     addItem: (state, action) => {
-    
-    },
-    removeItem: (state, action) => {
-    },
-    updateQuantity: (state, action) => {
+      const { name, image, cost } = action.payload;
+      const existingItem = state.items.find((item) => item.name === name);
 
-    
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        state.items.push({ name, image, cost, quantity: 1 });
+      }
+
+      state.numOfItems += 1;
+    },
+
+    removeItem: (state, action) => {
+      const { name, quantity } = action.payload;
+      state.items = state.items.filter((item) => item.name !== name);
+      state.numOfItems -= quantity;
+
+      if (state.numOfItems < 0) {
+        state.numOfItems = 0;
+      }
+    },
+
+    updateQuantity: (state, action) => {
+      const { name, quantity } = action.payload;
+      const existingItem = state.items.find((item) => item.name === name);
+
+      if (existingItem) {
+        const differenceQuantity = quantity - existingItem.quantity;
+        state.numOfItems += differenceQuantity;
+        existingItem.quantity = quantity;
+      }
     },
   },
 });
